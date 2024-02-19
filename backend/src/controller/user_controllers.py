3c2545c6b6f308faaab4623210c40ee85775cfdb
annalelsss <frozen001004@gmail.com>
2024-02-20 01:46:01 +0900

    signup

--- backend/src/controller/user_controllers.py
+++ backend/src/controller/user_controllers.py
@@ -1,12 +1,14 @@
 from fastapi import HTTPException
 from passlib.hash import bcrypt
-from pydantic import validator
 from ..db.connection import collection
 
 async def signup_user(user):
     existing_user = await collection.find_one({"email": user.email})
     if existing_user:
         raise HTTPException(400, "User already existed!")
+    existing_id = await collection.find_one({"id":user.id})
+    if existing_id:
+        raise HTTPException(400, "This Id is already used!")
     hashed_password = bcrypt.hash(user.password)
     document = {"name": user.name, "email": user.email, "id": user.id, "password": hashed_password}
     collection.insert_one(document)
