d9fbc8284d91b99a958fb87cbe84f7ce00be5552
Esther Yoo <estherliu919@gmail.com>
2024-02-20 19:47:01 +0900

    able to get a gpt response by chat_completion!

--- backend/src/db/connection.py
+++ backend/src/db/connection.py
@@ -1,10 +1,14 @@
+from fastapi import HTTPException
+from bson.objectid import ObjectId
 # MongoDB driver
 import motor.motor_asyncio
 from ..models.user import User
 from ..controller.chat_controller import (openai_config)
+import uuid
 
-password = 'GuNUsjOAYfcdvChC'
-MONGO_URL = f"mongodb+srv://test:{password}@cluster0.dy560h4.mongodb.net/"
+
+password = 'veGyue6hfhqurykH'
+MONGO_URL = f"mongodb+srv://estherliu919:{password}@cluster0.dy560h4.mongodb.net/"
 
 #이 파일과 mongoDB 연결
 client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URL)
@@ -18,18 +22,29 @@ async def create_user(user):
     result = await collection.insert_one(document)
     return document
 
-async def chat_completion(user, msg):
-    # grab chats of user
-    chats = [{"role": chat.role, "content": chat.content} for chat in user.chats]
-    chats.append({"content": msg, "role": "user"})
-    user.chats.append({"content": msg, "role": "user"})
-    print(user.chats)
+async def chat_completion(user_id: str, msg):
+    # Retrieve the user from the database
+    user = await collection.find_one({"id": user_id})
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    # grab chats of user to send
+    chats_to_send = [{"role": chat["role"], "content": chat["content"]} for chat in user["chats"]]
+    chats_to_send.append({"content": msg, "role": "user"})
+
+    # Update the user in the database with the new chat
+    new_chat = {"id": str(uuid.uuid4()),"content": msg, "role": "user"}
+    user["chats"].append(new_chat)
 
     # send all chats with new one to openAI API
     client = openai_config()
 
     # get latest response
-    chat_response = await client.chat.completions.create( model="gpt-3.5-turbo", messages=chats)
-    user.chats.append(chat_response.data.choices[0].message)
-    await user.save()
+    chat_response = client.chat.completions.create( model="gpt-3.5-turbo", messages=chats_to_send)
+    new_res = {"id": str(uuid.uuid4()),"content": chat_response.choices[0].message.content, "role": "assistant"}
+    user["chats"].append(new_res)
+    result = await collection.update_one(
+        {"_id": ObjectId(user["_id"])},
+        {"$set": {"chats": user["chats"]}}
+    )
     return user
