44ef068c25ce7efef82ed484829d6f55c5211699
minae-mina <124029562+minae-mina@users.noreply.github.com>
2024-02-27 20:29:40 +0900

    로그인과 회원가입화면에서 로고누르면 홈으로

--- frontend/src/components/Logo.jsx
+++ frontend/src/components/Logo.jsx
@@ -1,9 +1,20 @@
 import styles from "../css/Logo.module.css";
+import { Link, useNavigate } from "react-router-dom";
+import { Cookies } from "react-cookie";
 
 export default function Logo() {
+  const cookie = new Cookies();
+  const navigate = useNavigate();
+
+  const goHome = () => {
+    navigate("/");
+  };
+
   return (
     <div>
-      <h1 className={styles.logo}>CATCHUP</h1>
+      <h1 className={styles.logo} onClick={goHome}>
+        CATCHUP
+      </h1>
     </div>
   );
 }
