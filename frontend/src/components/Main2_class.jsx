602d9626eebc43e0abc9cd1c5f48bd0958e2a8c9
minae-mina <124029562+minae-mina@users.noreply.github.com>
2024-03-12 00:54:40 +0900

    dashboard 완성

--- frontend/src/components/Main2_class.jsx
+++ frontend/src/components/Main2_class.jsx
@@ -1,19 +1,46 @@
 //여기다가 테스트
 import React, { useState, useEffect } from "react";
 import styles from "../css/Main2.module.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
+import { Cookies } from "react-cookie";
 
 const Main2_class = () => {
+  const cookie = new Cookies();
+  const navigate = useNavigate();
+  const [classList, setClassList] = useState({});
+
+  const getClassList = async () => {
+    const token = cookie.get("token");
+    const headers = {
+      token: token,
+    };
+    await axios
+      .get("http://127.0.0.1:8000/user/dashboard", { headers })
+      .then((response) => {
+        console.log("생성 성공:", response.data);
+        // for (var key in response.data) {
+        //   console.log(key);
+        //   console.log(response.data[key]);
+        // }
+        setClassList(response.data);
+      })
+      .catch((error) => {
+        console.error("생성 실패:", error.response.data);
+      });
+  };
+
+  useEffect(() => {
+    getClassList();
+  }, []);
+
   return (
-    <div>
-      <div className={styles.classes}>
-        <Link to="/contentchat">
-          <div className={styles.classchat}>알고리즘</div>
+    <div className={styles.classes}>
+      {Object.entries(classList).map(([id, name]) => (
+        <Link to={`/chat/${id}`} key={id}>
+          <div className={styles.classchat}>{name}</div>
         </Link>
-        <div className={styles.classchat}>자료 구조</div>
-        <div className={styles.classchat}></div>
-      </div>
+      ))}
     </div>
   );
 };
