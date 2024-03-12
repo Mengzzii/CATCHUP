a1f6781f4d602493a98f1792c5591d63e6083f51
Esther Yoo <estherliu919@gmail.com>
2024-03-12 15:11:02 +0900

    Concept components loaded

--- backend/src/controller/chat_controller.py
+++ backend/src/controller/chat_controller.py
@@ -73,6 +73,37 @@ async def get_sample_chat(id:str, classroom_id:str):
     chats_to_send = [{"role": chat["role"], "content": chat["content"]} for chat in target_classroom["chatList"]]
     return chats_to_send
 
+async def get_concept_chat(id:str, classroom_id:str, concept_id:str):
+    user = await collection.find_one({"id":id})
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    
+    target_classroom = None
+    
+    #find classroom
+    for classroom in user["classroomList"]:
+        if classroom["classroomId"]==classroom_id:
+            target_classroom = classroom
+            break
+    
+    if target_classroom == None:
+        raise HTTPException(status_code=404, detail=f"Classroom ID '{classroom_id}' not found")
+    
+    target_concept = None
+
+    #find concept
+    for concept in target_classroom["conceptList"]:
+        if concept["conceptId"]==concept_id:
+            target_concept = concept
+            break
+    
+    if target_concept == None:
+        raise HTTPException(status_code=404, detail=f"Concept ID '{concept_id}' not found")
+    
+    chats_to_send = [{"role": chat["role"], "content": chat["content"]} for chat in target_concept["chatList"]]
+    return chats_to_send
+
+
 async def get_class_concepts(id:str, classroom_id:str):
     user = await collection.find_one({"id":id})
     if user is None:
@@ -92,7 +123,27 @@ async def get_class_concepts(id:str, classroom_id:str):
         concept_name_list.append(concept["name"])
     
     return concept_name_list
-    
+
+async def get_concept_list():
+    course_name = 'Computer Algorithm'
+    client = openai_config()
+    json_response = client.chat.completions.create(
+  model="gpt-4-1106-preview",
+  messages=[
+    {
+      "role": "user",
+      "content": f'''You are a Professor of a '{course_name}' course. Please generate a list of essential prerequisites of this course in  specific mathematical concepts, narrowed enough to be covered within 40 minutes. \n\nGuidelines to formatting:\n- Each concepts should be presented as a value in a JSON form. The key should be 'name'. \n- No  code block delimiter. No '\\n' or backslash('\') in output. \n- example :  [{{"name": "concept1"}}, {{"name": "concept2"}}, {{"name": "concept3"}}]'''
+    }
+  ],
+  temperature=0.2,
+  max_tokens=564,
+  top_p=1,
+  frequency_penalty=0,
+  presence_penalty=0
+)
+    print('!!!!!!!!!!!!!!!!!!')
+    print(json_response.choices[0].message.content)
+    return json_response.choices[0].message.content
 
 
 
