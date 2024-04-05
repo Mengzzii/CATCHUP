382efed721228755c014cac7405be765a3347234
믕지 <fronzen001004@gmail.com>
2024-04-06 02:43:12 +0900

    user input translate

--- backend/src/controller/langchainupdate.py
+++ backend/src/controller/langchainupdate.py
@@ -1,31 +1,34 @@
-# import os
-# from dotenv import load_dotenv
-# from langchain_openai import ChatOpenAI
-# from langchain_core.prompts import ChatPromptTemplate
-# from langchain_core.output_parsers import StrOutputParser
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
 
-# import streamlit as st
+import streamlit as st
+import os
+from dotenv import load_dotenv
 
-# os.environ["OPEN_API_KEY"] = os.getenv("OPEN_API_KEY")
-# os.environ["LANCHAIN_TRACING_V2"] = "true"
-# os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
+load_dotenv()
+os.environ["OPENAI_API_KEY"]=os.getenv("OPENAI_API_KEY")
 
-# ## Prompt Template
+## Langmith tracking
+os.environ["LANGCHAIN_TRACING_V2"]="true"
+os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
 
-# prompt = ChatPromptTemplate.from_messages(
-#     [
-#         ("system", "You are a helpful assistant. Please response to the user queries")
-#         ("user","Question: {question}")
-#     ]
-# )
+## Prompt Template
+prompt=ChatPromptTemplate.from_messages(
+    [
+        ("system","You are a helpful assistant. Please response to the user queries"),
+        ("user","Question:{question}")
+    ]
+)
 
-# ## Streamlit framework
-# st.title("Langchain Demo With OPENAI API")
-# input_text = st.text_input("Search the topic you want")
+## Streamlit framework
+st.title('Langchain Demo With OPENAI API')
+input_text=st.text_input("Search the topic u want")
 
-# ## OpenAI LLM
-# llm = ChatOpenAI(model="gpt-3.5-turbo")
-# output_parser = StrOutputParser()
-# chain = prompt | llm | output_parser
+## OpenAI LLM
+llm=ChatOpenAI(model="gpt-3.5-turbo")
+output_parser=StrOutputParser()
+chain=prompt|llm|output_parser
 
-# if input_text:
+if input_text:
+    st.write(chain.invoke({'question':input_text}))
