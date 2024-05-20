201ebb47b9bbf4d75872273166b0b5d8cabdf6e3
믕지 <fronzen001004@gmail.com>
2024-05-20 13:59:41 +0900

    vectordb END

--- backend/src/db/chromadb_connection.py
+++ backend/src/db/chromadb_connection.py
@@ -1,19 +1,19 @@
 import os
 from langchain_community.document_loaders.pdf import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import Chroma
-from vector_connection import vector_store, collection_material
+from vector_connection import resource, collection_material
 from dotenv import load_dotenv
 load_dotenv()
 
 os.environ['OPENAI_API_KEY'] = os.getenv("OPENAI_API_KEY")
 
-directory = "C:\\Users\\믕지\\OneDrive\\바탕 화면\\opencourseware\\기계학습수학"
+directory = ""
 
 # 1. PDF reader
-def load_docs(directory):
-    pdf_path = os.path.join(directory, "1.pdf")
+def load_docs(directory, num):
+    num_docs = num+".pdf"
+    pdf_path = os.path.join(directory, num_docs)
     loader = PyPDFLoader(pdf_path)
     docs = loader.load()
     return docs
@@ -24,45 +24,50 @@ def split_docs(docs):
     documents = text_splitter.split_documents(docs)
     return documents
 
+def split_page_contents(documents):
+    text_docs = []
+    for document in documents:
+        page_content = document.page_content
+        text_docs.append(page_content)
+    return text_docs
 
 ## 3. Vector Embedding and Vector Store
 def vectordb_store():
-    docs = load_docs(directory)
-    documents = split_docs(docs)
-    embeddings = OpenAIEmbeddings()
-    vector_store = Chroma.from_documents(
-        documents = documents, 
-        embedding = embeddings
-    )
-#     persist_directory = "chroma_db"
-#     vectordb = Chroma.from_documents(
-#         documents = documents, embedding = embeddings, persist_directory = persist_directory
-#      )
-    vector_store.persist()
-    # ids = [str(i) for i in range(len(documents))]
-    # collection_material.add(
-    #     documents=documents,
-    #     ids=ids
-    # )
-    return vector_store
+    for i in range(1,25):
+        docs = load_docs(directory, str(i))
+        documents = split_docs(docs)
+        text_docs = split_page_contents(documents)
+
+        ids = [f"{i}_{j}" for j in range(len(text_docs))]
+        metadatas = [{"catagory": ""} for k in range(len(text_docs))]
+        resource.add(
+            documents = text_docs,
+            ids = ids,
+            metadatas = metadatas
+        )
+        # print(str(i)+"의 embedding: "+str(len(text_docs)))
+    return resource
 
 # vectordb_store()
 
 def chromadb_main(concept):
     query = concept
-    result = vector_store.similarity_search(query)
-    return result
-    # result = collection_material.query(
-    # query_texts=[query],
-    # n_results=1
-    # )
 
-    # if result and 'documents' in result:
-    #     documents = result['documents']
-    #     document_content = documents[0][0]
-    #     return document_content
-    # else:
-    #     print("no result")
+    results_count = 4
+    result = resource.query(
+    query_texts = [query],
+    n_results = results_count
+    )
 
-result = chromadb_main("math")
-print(result)
+    document_content = ""
+    if result and 'documents' in result:
+        documents = result['documents']
+        for i in range(results_count):
+            document_content += documents[0][i] + "\n"
+    else:
+        print("no result")
+
+    return document_content
+
+# result = chromadb_main("""""")
+# print(result)
